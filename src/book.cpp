86108cb0d4106b148353f455667d095709846b9e
Facundo Gimenez <facu-judoca@hotmail.com.ar>
2022-07-14 16:51:17 -0300

    header files for book class

--- src/book.cpp
+++ src/book.cpp
@@ -1,43 +1,33 @@
+#include"book.h"
 #include<iostream>
-#include<string.h>
-
+#include<string>
 using std::string;
 
-class Book {
-    private:
-        string Name;
-        string Author;
-        int PublicationDate;
-        int Stock;
-        int Sales;
-    
-    public:
-        Book(string name,string author,int publicationData,int stock, int sales) {
-            Name = name;
-            Author = author;
-            Stock = stock;
-            Sales = sales;
-        }
-        void GetBookData () {
-            std::cout << Name << " - " << Author << std::endl;
-            std::cout << "Stock: " << Stock << std::endl;
-        }
-        
-        void AddNewStock(int newStock) {
-            Stock += newStock;
-        }
-        void RemoveStock(int quantity = 1) {
-            Stock -= quantity;
-        }
-        void SetStock(int stock) {
-            Stock = stock;
-        }
-        
-        void Sell(int quantity = 1) {
-            Sales += quantity;
-            RemoveStock(quantity);
-        }
-        void SetSales(int sales) {
-            Sales = sales;
-        }
-};
+Book::Book(string name,string author,int publicationData,int stock, int sales) {
+    Book::Name = name;
+    Book::Author = author;
+    Book::Stock = stock;
+    Book::Sales = sales;
+}
+void Book::GetBookData () {
+    std::cout << Name << " - " << Author << std::endl;
+    std::cout << "Stock: " << Stock << std::endl;
+}
+
+void Book::AddNewStock(int newStock) {
+    Stock += newStock;
+}
+void Book::RemoveStock(int quantity = 1) {
+    Stock -= quantity;
+}
+void Book::SetStock(int stock) {
+    Stock = stock;
+}
+
+void Book::Sell(int quantity = 1) {
+    Sales += quantity;
+    RemoveStock(quantity);
+}
+void Book::SetSales(int sales) {
+    Sales = sales;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
+#include"book.h"
 #include<iostream>
-#include<string.h>
-#include"book.hpp"
-
-using std::string;
 
 int main() {
+    std::cout << "Bookshop Management System" << std::endl;
     Book Comedia = Book("Comedia","Dante Aligheri", 10,10,10);
     Book ESDLB = Book("El Sabueso de los Baskerville", "Arthur Conan Doyle", 10,10,10);
+    Comedia.GetBookData();
+    ESDLB.GetBookData();
     return 0;
 }
